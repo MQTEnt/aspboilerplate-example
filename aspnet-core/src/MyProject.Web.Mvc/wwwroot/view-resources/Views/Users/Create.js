f9f936b32b54fb8e383fbee21b504f48860f45fa
mqtent <mqtent@gmail.com>
2019-08-29 10:53:38 +0700

    first commit

---
+++ aspnet-core/src/MyProject.Web.Mvc/wwwroot/view-resources/Views/Users/Create.js
@@ -0,0 +1,36 @@
+﻿(function() {
+    $(function () {
+        //Script for input mask
+        var $demoMaskedInput = $('.masked-input');
+
+        //Time
+        $demoMaskedInput.find('.time24').inputmask('hh:mm', { placeholder: '__:__ _m', alias: 'time24', hourFormat: '24' });
+
+        //Phone Number
+        $demoMaskedInput.find('.phone-number').inputmask('+99 (999) 999-99-99', { placeholder: '+__ (___) ___-__-__' });
+
+        //Price (Dollar Money)
+        $demoMaskedInput.find('.money-dollar').inputmask('99,99 $', { placeholder: '__,__ $' });
+
+        //Credit Card
+        $demoMaskedInput.find('.credit-card').inputmask('9999 9999 9999 9999', { placeholder: '____ ____ ____ ____' });
+
+        //Email
+        $demoMaskedInput.find('.email').inputmask({ alias: "email" });
+
+        //Clear search
+        var btnClear = $('#btnClear');
+        btnClear.click(function () {
+            $('.form-control').val('');
+            $('input[type=checkbox]').prop("checked", false);
+        });
+
+        //Script for datepicker
+        $('.datepicker').bootstrapMaterialDatePicker({
+            format: 'dddd DD MMMM YYYY',
+            clearButton: true,
+            weekStart: 1,
+            time: false
+        });
+    });
+})();
---
+++ aspnet-core/src/MyProject.Web.Mvc/wwwroot/view-resources/Views/Users/Index.js
@@ -0,0 +1,124 @@
+﻿(function() {
+    $(function() {
+
+        var _userService = abp.services.app.user;
+        var _$modal = $('#UserCreateModal');
+        var _$form = _$modal.find('form');
+
+        _$form.validate({
+            rules: {
+                Password: "required",
+                ConfirmPassword: {
+                    equalTo: "#Password"
+                }
+            }
+        });
+
+        $('#RefreshButton').click(function () {
+            refreshUserList();
+        });
+
+        $('.delete-user').click(function () {
+            var userId = $(this).attr("data-user-id");
+            var userName = $(this).attr('data-user-name');
+
+            deleteUser(userId, userName);
+        });
+
+        $('.edit-user').click(function (e) {
+            var userId = $(this).attr("data-user-id");
+
+            e.preventDefault();
+            $.ajax({
+                url: abp.appPath + 'Users/EditUserModal?userId=' + userId,
+                type: 'POST',
+                contentType: 'application/html',
+                success: function (content) {
+                    $('#UserEditModal div.modal-content').html(content);
+                },
+                error: function (e) { }
+            });
+        });
+
+        _$form.find('button[type="submit"]').click(function (e) {
+            e.preventDefault();
+
+            if (!_$form.valid()) {
+                return;
+            }
+
+            var user = _$form.serializeFormToObject(); //serializeFormToObject is defined in main.js
+            user.roleNames = [];
+            var _$roleCheckboxes = $("input[name='role']:checked");
+            if (_$roleCheckboxes) {
+                for (var roleIndex = 0; roleIndex < _$roleCheckboxes.length; roleIndex++) {
+                    var _$roleCheckbox = $(_$roleCheckboxes[roleIndex]);
+                    user.roleNames.push(_$roleCheckbox.val());
+                }
+            }
+
+            abp.ui.setBusy(_$modal);
+            _userService.create(user).done(function () {
+                _$modal.modal('hide');
+                location.reload(true); //reload page to see new user!
+            }).always(function () {
+                abp.ui.clearBusy(_$modal);
+            });
+        });
+        
+        _$modal.on('shown.bs.modal', function () {
+            _$modal.find('input:not([type=hidden]):first').focus();
+        });
+
+        function refreshUserList() {
+            location.reload(true); //reload page to see new user!
+        }
+
+        function deleteUser(userId, userName) {
+            abp.message.confirm(
+                abp.utils.formatString(abp.localization.localize('AreYouSureWantToDelete', 'MyProject'), userName),
+                function (isConfirmed) {
+                    if (isConfirmed) {
+                        _userService.delete({
+                            id: userId
+                        }).done(function () {
+                            refreshUserList();
+                        });
+                    }
+                }
+            );
+        }
+
+        //Script for input mask
+        var $demoMaskedInput = $('.masked-input');
+
+        //Time
+        $demoMaskedInput.find('.time24').inputmask('hh:mm', { placeholder: '__:__ _m', alias: 'time24', hourFormat: '24' });
+
+        //Phone Number
+        $demoMaskedInput.find('.phone-number').inputmask('+99 (999) 999-99-99', { placeholder: '+__ (___) ___-__-__' });
+
+        //Price (Dollar Money)
+        $demoMaskedInput.find('.money-dollar').inputmask('99,99 $', { placeholder: '__,__ $' });
+
+        //Credit Card
+        $demoMaskedInput.find('.credit-card').inputmask('9999 9999 9999 9999', { placeholder: '____ ____ ____ ____' });
+
+        //Email
+        $demoMaskedInput.find('.email').inputmask({ alias: "email" });
+
+        //Clear search
+        var btnClear = $('#btnClear');
+        btnClear.click(function () {
+            $('.form-control').val('');
+        });
+
+        //Script for datepicker
+        $('.datepicker').bootstrapMaterialDatePicker({
+            format: 'dddd DD MMMM YYYY',
+            clearButton: true,
+            weekStart: 1,
+            time: false
+        });
+    });
+})();
